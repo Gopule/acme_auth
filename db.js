77bc53ce18f74ee5e6864e25d787c8e45aac33f8
Altus Szawlowski <altusszawlowski@gmail.com>
2021-04-05 17:25:06 -0400

    bcrypt finished

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ const { STRING } = Sequelize;
 const config = {
   logging: false,
 };
-const secret_key = process.env.JWT
+const secret_key = process.env.JWT;
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 if (process.env.LOGGING) {
   delete config.logging;
@@ -21,11 +22,11 @@ const User = conn.define("user", {
 
 User.byToken = async (token) => {
   try {
-    console.log('this is the output of byToken -->', token)
-    const verified = await jwt.verify(token, secret_key)
-    console.log('verified', verified)
+    console.log("this is the output of byToken -->", token);
+    const verified = await jwt.verify(token, secret_key);
+    console.log("verified", verified);
     const user = await User.findByPk(verified);
-    console.log(user)
+    console.log(user);
     if (user) {
       return user;
     }
@@ -40,19 +41,41 @@ User.byToken = async (token) => {
 };
 
 User.authenticate = async ({ username, password }) => {
+  // const hashedPassword = await hashPassword(password);
+  // password = hashedPassword;
   const user = await User.findOne({
     where: {
       username,
-      password,
     },
   });
-  if (user) {
+  if (!user) {
+    throw "no";
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (match) {
     return user.id;
   }
   const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
+
+async function hashPassword(password) {
+  const saltCount = 5;
+  return await bcrypt.hash(password, saltCount);
+}
+
+// bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+//   // result == true
+// });
+// bcrypt.compare((someOtherPlaintextPassword, hash) {
+//   // result == false
+// });
+
+User.beforeCreate(async (user) => {
+  const hashedPassword = await hashPassword(user.password);
+  user.password = hashedPassword;
+});
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
